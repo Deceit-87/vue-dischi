57ca56ba9d7cd413f5b0a0234385f7d157c0cad0
Deceit-87 <manuelfreund318@gmail.com>
2022-03-13 16:18:25 +0100

    album

---
+++ src/components/AlbumCard.vue
@@ -0,0 +1,37 @@
+<template>
+  <li class="card-wrapper">
+      <figure class="card-thumb">
+
+          <img :src="album.poster" alt="">
+
+      </figure>
+      <div class="card-info">
+
+          <h3>
+              {{album.title}}
+          </h3>
+          <p>
+              {{album.author}}
+          </p>
+          <p>
+              {{album.year}}
+          </p>
+
+      </div>
+  </li>
+</template>
+
+<script>
+export default {
+    props:{
+       album:{
+        type: Object,
+        required: true,
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/MainSpotify.vue
@@ -0,0 +1,62 @@
+<template>
+  <div class="album-list">
+    <ul>
+
+         <Album 
+         v-for="(album,index) in albums"
+          :key="index"
+          :album="album"/>
+
+    </ul>
+
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+
+import Album from './AlbumCard.vue'
+
+export default {
+    components:{
+        Album,
+    },    
+  data(){
+      return{
+          albums:[],
+  
+      }
+   },
+   methods: {
+       fetchAlbums:function(){
+           axios.get('https://flynn.boolean.careers/exercises/api/array/music')
+           .then( res =>{
+               this.albums = res.data.response;
+               console.log(this.albums);
+
+           })
+        //    .catch(err =>{
+        //        console.error(err.response);
+        //        this.albums = [];
+        //    })
+         
+          
+        }
+  },
+  created(){
+        this.fetchAlbums();
+  
+  },
+}
+</script>
+
+<style>
+
+.album-list{
+    width: 1155px;
+    margin: 0 auto;
+    height: 750px;
+    border: 2px solid black;
+    margin-top: 80px;
+}
+</style>
