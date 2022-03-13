2bae286bb3c6a8f777fd9ba140c1263ddb903450
Deceit-87 <manuelfreund318@gmail.com>
2022-03-13 22:36:08 +0100

    aggiunte opzioni dinamiche , non funzionanti

--- src/components/MainSpotify.vue
+++ src/components/MainSpotify.vue
@@ -3,12 +3,27 @@
     <ul class="album-list">
 
          <Album 
-         v-for="(album,index) in albums"
+         v-for="(album,index) in setFilter()"
           :key="index"
           :patate="album"/>
 
     </ul>
 
+    <div class="select-album">
+
+      <select 
+      v-model="sctAlbums" 
+      name="genre">
+        <option 
+        v-for="(album,genre) in albums"
+        :key="genre"
+        :value="album.genre">{{album.genre}}
+        </option>
+       
+      </select>
+
+    </div>
+ 
   </div>
 </template>
 
@@ -24,7 +39,8 @@ export default {
   data(){
       return{
           albums:[],
-  
+          sctAlbums: '',
+
       }
    },
    methods: {
@@ -32,17 +48,58 @@ export default {
            axios.get('https://flynn.boolean.careers/exercises/api/array/music')
            .then( res =>{
                this.albums = res.data.response;
-               console.log(this.albums);
 
            })
-        //    .catch(err =>{
-        //        console.error(err.response);
-        //        this.albums = [];
-        //    })
+           .catch(err =>{
+               console.error(err.response);
+               this.albums = [];
+           })
          
           
-        }
-  },
+        },
+        setFilter:function(){
+               let albumFiltred = [];
+               for (let i = 0; i < this.albums.length; i++) {
+                    if (( this.sctAlbums != "") && ( this.sctAlbums != "All")) {
+                        if ( this.albums[i].genre == this.sctAlbums ) {
+                          albumFiltred.push( this.albums[i] );
+                        }
+                        else {
+                          this.album = albumFiltred;
+                          }
+                        } 
+                    else {
+                       albumFiltred.push( this.albums[i] );
+                    }
+               }
+               return albumFiltred;
+         },
+              genreArray:function(){
+                console.log(this.albums.genre)
+              },
+           
+        //  genreArray:function(){
+        //    let selectOpt = [];
+        //     let genre = this.albums.genre;
+           
+
+        //    for (let i = 0; i < this.albums.length; i++) {
+        //      if(selectOpt.includes(genre[i]) === false){
+        //        selectOpt.push(genre[i]);
+        //       console.log( selectOpt)
+        //      }
+        //      else{
+        //        return selectOpt;
+
+        //      }
+        //                    console.log( selectOpt)
+
+        //    }
+           
+        //  },
+
+    },
+
   created(){
         this.fetchAlbums();
   
@@ -50,7 +107,14 @@ export default {
 }
 </script>
 
-<style>
+<style lang="scss" scoped>
+
+.select-album{
+    position: absolute;
+    top: 2%;
+    left: 50%;
+    transform: translatex(-50%);
+}
 
 .album-container{
     display: flex;
